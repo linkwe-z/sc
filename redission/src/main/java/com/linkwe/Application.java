eba20cbb47f8a53de0a60a4c7952b08e72cf095f
zenglingwei <lwzeng@iyouxin.com>
2018-01-26 15:50:11 +0800

    four

---
+++ redission/src/main/java/com/linkwe/Application.java
@@ -0,0 +1,17 @@
+package com.linkwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by lwz on 2018/1/26.
+ * desc:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
